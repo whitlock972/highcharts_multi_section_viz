0a2437543554654b33beaf863153c7a85236e5b9
Alex Unger <alexander.unger@betterup.co>
2020-10-14 10:48:07 -0700

    with baseline only show colored lines

--- src/visualizations/wpm_column_chart.ts
+++ src/visualizations/wpm_column_chart.ts
@@ -1,12 +1,10 @@
-import { Looker, VisualizationDefinition, LookerChartUtils } from '../common/types';
-import { handleErrors, formatType } from '../common/utils';
+import { Looker, VisualizationDefinition } from '../common/types';
+import { handleErrors } from '../common/utils';
 import './wpm_column_chart.css';
 import * as Highcharts1 from 'highcharts';
-import { stockChart }  from 'highcharts/highstock';
-import { Chart, Options, charts, XAxisOptions, SeriesOptionsType, SeriesColumnOptions } from 'highcharts';
+import { Chart } from 'highcharts';
 
 declare var looker: Looker;
-let chart: Chart;
 let chartOptions: any;
 
 let Highcharts:any = Highcharts1
@@ -74,9 +72,7 @@ const vis: CustomColumnViz = {
     },
     // Set up the initial state of the visualization
     create(element, config) {
- 
-
-        element.innerHTML = "Rendering ..."
+        element.innerHTML = "Rendering ...";
     },
     // Render in response to the data or settings changing
     updateAsync(data, element, config, queryResponse, details, done) {
@@ -95,12 +91,12 @@ const vis: CustomColumnViz = {
             let key = field.label
             let value = field.name
             return { [key]: value }
-        })
+        });
         let dimensions = queryResponse.fields.dimension_like.map((field) => {
             let key = field.label
             let value = field.name
             return { [key]: value }
-        })
+        });
         let options = this.options;
         options["domain"] =
         {
@@ -110,16 +106,7 @@ const vis: CustomColumnViz = {
             display: "select",
             values: dimensions,
             order: 1
-        }
-        options["secondCategory"] =
-        {
-            section: "X-Axis",
-            type: "string",
-            label: "Column Label",
-            display: "select",
-            values: dimensions,
-            order: 2
-        }
+        };
         options["firstCategory"] =
         {
             section: "X-Axis",
@@ -127,8 +114,17 @@ const vis: CustomColumnViz = {
             label: "Column Group Label",
             display: "select",
             values: dimensions,
+            order: 2
+        };
+        options["secondCategory"] =
+        {
+            section: "X-Axis",
+            type: "string",
+            label: "Column Label",
+            display: "select",
+            values: dimensions,
             order: 3
-        }
+        };
         options["benchmarkMeasure"] =
         {
             section: "Y-Axis",
@@ -137,14 +133,14 @@ const vis: CustomColumnViz = {
             display: "select",
             values: measures,
             order: 1
-        }
+        };
         options["benchmarkIcon"] = {
             section: "Y-Axis",
             type: "string",
             label: "Benchmark Point Icon URL",
             default: 'https://freesvg.org/img/line-drawn.png',
             order: 2
-        }
+        };
         options["baselineMeasure"] =
         {
             section: "Y-Axis",
@@ -153,7 +149,7 @@ const vis: CustomColumnViz = {
             display: "select",
             values: measures,
             order: 3
-        }
+        };
         options["reflectionPoint1Measure"] =
         {
             section: "Y-Axis",
@@ -162,7 +158,7 @@ const vis: CustomColumnViz = {
             display: "select",
             values: measures,
             order: 4
-        }
+        };
         options["reflectionPoint2Measure"] =
         {
             section: "Y-Axis",
@@ -171,7 +167,7 @@ const vis: CustomColumnViz = {
             display: "select",
             values: measures,
             order: 5
-        }
+        };
         options["title"] =
         {
             section: "Labels",
@@ -179,21 +175,21 @@ const vis: CustomColumnViz = {
             label: "Title",
             placeholder: "Whole Person Model",
             order: 1
-        }
+        };
         options["subtitle"] =
         {
             section: "Labels",
             type: "string",
             label: "Subtitle",
             order: 2
-        }
+        };
         options["border"] =
         {
             section: "Labels",
             type: "boolean",
             label: "Show Column Group Label Border",
             order: 3
-        }
+        };
         options["borderBoxColor"] =
         {
             section: "Labels",
@@ -202,7 +198,7 @@ const vis: CustomColumnViz = {
             display: "color",
             default: "coral",
             order: 4
-        }
+        };
         options["borderFontSize"] =
         {
             section: "Labels",
@@ -211,7 +207,7 @@ const vis: CustomColumnViz = {
             placeholder: "16px",
             default: "16px",
             order: 5
-        }
+        };
         options["decimalPrecision"] =
         {
             section: "Labels",
@@ -220,7 +216,7 @@ const vis: CustomColumnViz = {
             label: "Decimal Precision",
             default: 0,
             order: 6
-        }
+        };
         options["series1LegendColor"] =
         {
             section: "Labels",
@@ -228,7 +224,7 @@ const vis: CustomColumnViz = {
             label: "RP 1 Legend Color",
             display: "color",
             order: 7
-        }
+        };
         options["series2LegendColor"] =
         {
             section: "Labels",
@@ -236,17 +232,17 @@ const vis: CustomColumnViz = {
             label: "RP 2 Legend Color",
             display: "color",
             order: 8
-        }
+        };
 
-        this.trigger('registerOptions', options) // register options with parent page to update visConfig
+        this.trigger('registerOptions', options); // register options with parent page to update visConfig
 
         if (!config.domain) {
             done();
             return;
         }
 
-        chartOptions.title = config.title
-        chartOptions.subtitle = config.subtitle
+        chartOptions.title = config.title;
+        chartOptions.subtitle = config.subtitle;
 
         let xCategories: Array<string> = [];
         let baselineSeriesValues: Array<any> = [];
@@ -268,13 +264,27 @@ const vis: CustomColumnViz = {
             var secondRPcolor = lookupColor(domainCell.value);
             xCategories.push(
                 secondCategoryCell.value
-            )
-            baselineSeriesValues.push(
-                [ i, rounder(baselineMeasureCell.value,config.decimalPrecision)]
-            )
+            );
+
+            if (showRP1) {
+                baselineSeriesValues.push(
+                    { x: i, y: rounder(baselineMeasureCell.value,config.decimalPrecision),
+                        color: "#98A4B7"
+                    }
+                );
+            }
+            else {
+                baselineSeriesValues.push(
+                    { x: i, y: rounder(baselineMeasureCell.value,config.decimalPrecision),
+                        color: color,
+                        className: firstCategoryCell.value.replace(/\s/g, '_')
+                    }
+                );
+            }
+
             benchmarkSeriesValues.push(
                 [i, rounder(benchmarkMeasureCell.value,config.decimalPrecision)]
-            )
+            );
             if (showRP1) {
                 var reflectionPoint1Cell = row[config.reflectionPoint1Measure];
                 reflectionPoint1SeriesValues.push(
@@ -282,7 +292,7 @@ const vis: CustomColumnViz = {
                         , color: color
                         , className: firstCategoryCell.value.replace(/\s/g, '_')
                     }
-                )
+                );
             }
             if (showRP2) {
                 var reflectionPoint2Cell = row[config.reflectionPoint2Measure];
@@ -291,15 +301,14 @@ const vis: CustomColumnViz = {
                         , color: secondRPcolor
                         , className: secondCategoryCell.value.replace(/\s/g, '_')
                     }
-                )
+                );
             }
 
-            primaryLabelClasses.push(firstCategoryCell.value.replace(/\s/g, '_'))
+            primaryLabelClasses.push(firstCategoryCell.value.replace(/\s/g, '_'));
         });
 
         let baselineSeries : any = {};
-        baselineSeries.name = "Baseline"
-        baselineSeries.color = "#98A4B7"
+        baselineSeries.name = "Baseline";
         baselineSeries.dataLabels = {
                             enabled: true,
                             inside: true,
@@ -307,25 +316,25 @@ const vis: CustomColumnViz = {
                             style: {
                                 textOutline: 'none'
                             }
-                        }
-        baselineSeries.data = baselineSeriesValues
+                        };
+        baselineSeries.data = baselineSeriesValues;
 
         let numberOfClasses: number = primaryLabelClasses.length;
         
         let benchmarkSeries : any = {};
-        benchmarkSeries.name = "Benchmark"
+        benchmarkSeries.name = "Benchmark";
         benchmarkSeries.type = "scatter";
         benchmarkSeries.marker = {width: 0.7*totalWidth/numberOfClasses,
               height: 25,
-              symbol: `url(${config.benchmarkIcon})`}
+              symbol: `url(${config.benchmarkIcon})`};
         benchmarkSeries.data = benchmarkSeriesValues ;
 
         let reflectionPoint1Series : any = {};
         if (showRP1) {
             reflectionPoint1Series.name = 
                 showRP2 ? 'Reflection Point 1' : 'Reflection Point'; 
-            reflectionPoint1Series.data = reflectionPoint1SeriesValues
-            reflectionPoint1Series.color = `${config.series1LegendColor}`
+            reflectionPoint1Series.data = reflectionPoint1SeriesValues;
+            reflectionPoint1Series.color = `${config.series1LegendColor}`;
 
             reflectionPoint1Series.dataLabels = {
                 enabled: true,
@@ -334,14 +343,14 @@ const vis: CustomColumnViz = {
                 style: {
                     textOutline: 'none'
                 }
-            }
+            };
 
         }
         let reflectionPoint2Series : any = {};
         if (showRP1 && showRP2) {
-            reflectionPoint2Series.name = 'Reflection Point 2'
-            reflectionPoint2Series.data = reflectionPoint2SeriesValues
-            reflectionPoint2Series.color = `${config.series2LegendColor}`
+            reflectionPoint2Series.name = 'Reflection Point 2';
+            reflectionPoint2Series.data = reflectionPoint2SeriesValues;
+            reflectionPoint2Series.color = `${config.series2LegendColor}`;
 
             reflectionPoint2Series.dataLabels = {
                 enabled: true,
@@ -350,18 +359,18 @@ const vis: CustomColumnViz = {
                 style: {
                     textOutline: 'none'
                 }
-            }
+            };
         }
        
 
-        chartOptions = baseChartOptions
-        chartOptions.xAxis.categories =  xCategories
+        chartOptions = baseChartOptions;
+        chartOptions.xAxis.categories =  xCategories;
         if (showRP2 && showRP1) {
-            chartOptions.series = [baselineSeries, reflectionPoint1Series, reflectionPoint2Series, benchmarkSeries]
+            chartOptions.series = [baselineSeries, reflectionPoint1Series, reflectionPoint2Series, benchmarkSeries];
         } else if (showRP1) {
-            chartOptions.series = [baselineSeries, reflectionPoint1Series, benchmarkSeries]
+            chartOptions.series = [baselineSeries, reflectionPoint1Series, benchmarkSeries];
         } else {
-            chartOptions.series = [baselineSeries, benchmarkSeries]    
+            chartOptions.series = [baselineSeries, benchmarkSeries];
         }
         var vizDiv = document.createElement('div');
         vizDiv.setAttribute('id','viz');
@@ -375,19 +384,21 @@ const vis: CustomColumnViz = {
 
         let uniquePrimaryLabelClasses: Array<string> = [...new Set(primaryLabelClasses)]; 
         let leftMargin: number = 100;
-        let widthIncrement = (totalWidth - leftMargin)/numberOfClasses    
+        let widthIncrement = (totalWidth - leftMargin)/numberOfClasses; 
         let styles: string = '';
 
         uniquePrimaryLabelClasses.forEach( (className:string, i: number) => {
-            let numberOfElements: number = primaryLabelClasses.filter(x => x==className).length
+            let numberOfElements: number = primaryLabelClasses.filter(x => x==className).length;
             let width = widthIncrement*numberOfElements;
-            width = width -10
+            width = width -10;
             
-            let newLabelElement = document.createElement('div')
-            newLabelElement.setAttribute("id",className)
+            let newLabelElement = document.createElement('div');
+            newLabelElement.setAttribute("id",className);
             let domainName = getDomainNameFromPrimaryLabel(className,config,data).trim();
-            let labelValue = className.replace('_',' ').trim()
-            if (domainName != labelValue) { labelValue = labelValue.replace(domainName,'').trim() } 
+            let labelValue = className.replace('_',' ').trim();
+            if (domainName != labelValue) {
+                labelValue = labelValue.replace(domainName,'').trim();
+            } 
             newLabelElement.innerHTML = labelValue;
             let borderStyle = config.border ? `2px solid ${config.borderBoxColor}` : 'none';
             styles += `#${className} {
@@ -402,26 +413,26 @@ const vis: CustomColumnViz = {
                 margin-right:${i==numberOfClasses?"10px":""};
                 color:${lookupColor(domainName)};
             }
-            `
-            labelDivs.push(newLabelElement)
+            `;
+            labelDivs.push(newLabelElement);
         })
         
 
         var styleEl = document.createElement('style');
-        styleEl.setAttribute('type',"text/css")
+        styleEl.setAttribute('type',"text/css");
         styles +=  `
         @font-face {font-family: "Gilroy"; src: url("//db.onlinewebfonts.com/t/1dc8ecd8056a5ea7aa7de1db42b5b639.eot"); src: url("//db.onlinewebfonts.com/t/1dc8ecd8056a5ea7aa7de1db42b5b639.eot?#iefix") format("embedded-opentype"), url("//db.onlinewebfonts.com/t/1dc8ecd8056a5ea7aa7de1db42b5b639.woff2") format("woff2"), url("//db.onlinewebfonts.com/t/1dc8ecd8056a5ea7aa7de1db42b5b639.woff") format("woff"), url("//db.onlinewebfonts.com/t/1dc8ecd8056a5ea7aa7de1db42b5b639.ttf") format("truetype"), url("//db.onlinewebfonts.com/t/1dc8ecd8056a5ea7aa7de1db42b5b639.svg#Gilroy") format("svg"); }  
         div {
             font-family: "Gilroy"
         }
-        `
-        styleEl.innerHTML = styles 
+        `;
+        styleEl.innerHTML = styles;
         document.head.appendChild(styleEl);
 
         var customLabelsDiv: Element = document.createElement('div');
-        customLabelsDiv.setAttribute('class', 'customLabels')
-        customLabelsDiv.setAttribute('style',"display: flex")
-        labelDivs.forEach(x => customLabelsDiv.appendChild(x))
+        customLabelsDiv.setAttribute('class', 'customLabels');
+        customLabelsDiv.setAttribute('style',"display: flex");
+        labelDivs.forEach(x => customLabelsDiv.appendChild(x));
         element.appendChild(customLabelsDiv);
        
         done();
@@ -444,7 +455,7 @@ function lookupColor(domainName: string): string {
         break;
         default: color = "#2B333F"
     }
-    return color
+    return color;
 }
 
 function lookupSecondaryColor(domainName: string):string {
@@ -468,19 +479,19 @@ return color;
 
 
 function rounder(float:number, digits:number): number {
-    let rounded = Math.round(float * 10**digits) / 10**digits
-    return rounded
+    let rounded = Math.round(float * 10**digits) / 10**digits;
+    return rounded;
 }
 
 function getDomainNameFromPrimaryLabel(className:string,config:any,data:any):string {
-    let domainName: string = '' 
+    let domainName: string = '' ;
     if(config.firstCategory == config.domain) {
-        domainName = className
+        domainName = className;
     }
     else {
-        domainName = data[0][config.domain].value
+        domainName = data[0][config.domain].value;
     }
-    return domainName
+    return domainName;
 }
 
 looker.plugins.visualizations.add(vis);
